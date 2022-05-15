9b62b17b5b71e59427fa395e0bdadb71d7554117
Arian Huapaya <arian.huapaya@gmail.com>
2022-05-15 15:54:13 +0200

    Checkboxes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// import Header from './components/layout/Header';
+// import Content from './components/layout/Content';
+// import Footer from './components/layout/Footer';
+// import { Container } from 'react-bootstrap';
+
+class App extends React.Component {
+  render(){
+  return  <div className='App'>
+        Noise level<input type="checkbox" name="Noise" value = "Noise"/>
+        Complaints<input type="checkbox" name="Noise" value = "Noise"/>
+        Car contamination<input type="checkbox" name="Noise" value = "Noise"/>
+        </div>
+        
   }
 }
-
-export default withAuthenticator(App);
+export default App;
