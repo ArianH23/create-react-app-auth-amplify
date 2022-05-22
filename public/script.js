c92ea1b487664b08c5ef06b014e15f62b965cc09
Arian Huapaya <arian.huapaya@gmail.com>
2022-05-22 18:26:15 +0200

    Displaying list of neighbourhoods

---
+++ public/script.js
@@ -0,0 +1,56 @@
+
+const url = "https://4xthlyydt2.execute-api.eu-west-1.amazonaws.com/production/sample";
+
+
+//get the current status of votting
+function get() {
+
+    $.ajax({
+    url: url,
+    type: 'get',
+    async: false,
+    success: function( data, textStatus, jQxhr ){
+        var neighbourhoods = [];
+
+        for (var i=0; i < data.body.length; i++) {
+            var neigh = [data.body[i].name, data.body[i].score, data.body[i].rank];
+            neighbourhoods.push(neigh);
+        }
+        console.log(neighbourhoods)
+
+        
+        for (var i=0; i < data.body.length; i++) {
+            document.getElementById('neighbourhood-name').innerHTML += '<li>' + neighbourhoods[i][0] + '</li>';
+            document.getElementById('neighbourhood-scores').innerHTML += '<li>' + neighbourhoods[i][1] +'</li>';
+            document.getElementById('neighbourhood-rank').innerHTML += '<li>' + neighbourhoods[i][2] + '</li>';
+        }
+        
+
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log("lmao");
+        console.log( errorThrown );
+    }
+    });
+
+}
+
+
+// Will need to get updated inthe future
+function update(clicked_id) {
+    $.ajax({
+    url: url,
+    type: 'get',
+    async: false,
+    success: function( data, textStatus, jQxhr ){
+        document.getElementById(text).value=data.body.Count;
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
+    }
+    });
+}
+			
+			
+
+
